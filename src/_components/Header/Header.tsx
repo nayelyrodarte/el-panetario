1e635575c694bb1af470f997e0838195b78b776b
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-02-05 16:49:53 -0500

    Initial styling for header

--- src/_components/Header/Header.tsx
+++ src/_components/Header/Header.tsx
@@ -1,5 +1,25 @@
 import styles from './Header.module.css';
+import Image from 'next/image';
+import Croissant from './../../_assets/svg/croissant_beige.svg';
 
 export default function Header() {
-  return <nav className={styles.header}>Header</nav>;
+  return (
+    <nav className={styles.header}>
+      <section className={styles.header__intro}>
+        <Image
+          width={40}
+          height={40}
+          src={Croissant}
+          alt="Icono de croissant"
+        />
+        <p>El Panetario</p>
+      </section>
+      <section>
+        <ul className={styles.header__links}>
+          <li>Inicio</li>
+          <li>Recetas</li>
+        </ul>
+      </section>
+    </nav>
+  );
 }
