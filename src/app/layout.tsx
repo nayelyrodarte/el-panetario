1ba14d329575a7baca92abeab94e09f218a57ab6
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-02-05 14:56:01 -0500

    Fix project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Header from '../_components/Header/Header';
 
 export const metadata: Metadata = {
   title: 'El Panetario',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import styles from './page.module.css';
+import Header from '../_components/Header/Header';
 
 export default function Home() {
   return (
