b280a57b6a191ff99fe480959f03d3809cd52edd
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-02-06 11:58:36 -0500

    Add styling for header (desktop)

--- src/_components/Header/Header.tsx
+++ src/_components/Header/Header.tsx
@@ -1,42 +1,45 @@
 'use client';
 
-import styles from './Header.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+
 import Croissant from './../../_assets/svg/croissant_beige.svg';
+import styles from './Header.module.scss';
 
 export default function Header() {
   const path = usePathname();
   return (
     <nav className={styles.header}>
-      <section>
-        <Image
-          width={40}
-          height={40}
-          src={Croissant}
-          alt="Icono de croissant"
-        />
-        <p>El Panetario</p>
-      </section>
-      <section>
-        <ul>
-          <li
-            style={{
-              textDecoration: `${path === '/' ? 'underline' : 'none'}`,
-            }}
-          >
-            <Link href="/">Inicio</Link>
-          </li>
-          <li
-            style={{
-              textDecoration: `${path === '/recipes' ? 'underline' : 'none'}`,
-            }}
-          >
-            <Link href="/recipes">Recetas</Link>
-          </li>
-        </ul>
-      </section>
+      <div className="header__inner">
+        <section>
+          <Image
+            width={40}
+            height={40}
+            src={Croissant}
+            alt="Icono de croissant"
+          />
+          <h3>El Panetario</h3>
+        </section>
+        <section>
+          <ul>
+            <li
+              style={{
+                textDecoration: `${path === '/' ? 'underline' : 'none'}`,
+              }}
+            >
+              <Link href="/">Inicio</Link>
+            </li>
+            <li
+              style={{
+                textDecoration: `${path === '/recipes' ? 'underline' : 'none'}`,
+              }}
+            >
+              <Link href="/recipes">Recetas</Link>
+            </li>
+          </ul>
+        </section>
+      </div>
     </nav>
   );
 }
