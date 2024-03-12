b38c38c44f9516bb0067731d690751edb639c31a
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-03-12 16:38:03 -0400

    Remove unused sass files

--- src/_components/Footer/Footer.tsx
+++ src/_components/Footer/Footer.tsx
@@ -1,14 +1,30 @@
-import styles from './Footer.module.scss';
+'use client';
+
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  background-color: var(--primary-color);
+  color: var(--neutral-color);
+
+  width: 100%;
+  min-height: 5rem;
+
+  padding: var(--padding);
+
+  section {
+    max-width: var(--max-width);
+  }
+`;
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
+    <StyledFooter>
       <section>
         <div>
           <h2>El Panetario</h2>
           <p>© 2024. Todos los derechos reservados.</p>
         </div>
       </section>
-    </footer>
+    </StyledFooter>
   );
 }
--- src/_components/Header/Header.tsx
+++ src/_components/Header/Header.tsx
@@ -3,14 +3,58 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import styled from 'styled-components';
 
 import Croissant from './../../_assets/svg/croissant_beige.svg';
-import styles from './Header.module.scss';
+
+const StyledHeader = styled.nav`
+  background-color: var(--primary-color);
+  color: var(--neutral-color);
+
+  padding: var(--padding);
+  width: 100%;
+
+  div {
+    max-width: var(--max-width);
+
+    margin: auto;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1rem;
+
+    font-family: var(--primary-font);
+  }
+
+  ul {
+    font-family: var(--secondary-font);
+
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+`;
 
 export default function Header() {
   const path = usePathname();
   return (
-    <nav className={styles.header}>
+    <StyledHeader>
       <div className="header__inner">
         <section>
           <Image
@@ -40,6 +84,6 @@ export default function Header() {
           </ul>
         </section>
       </div>
-    </nav>
+    </StyledHeader>
   );
 }
