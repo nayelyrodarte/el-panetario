eff0a0e6b3b9c21ff4340b5a1d3ca5467da8ba7e
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-04-22 14:36:24 -0400

    Remove unused import

--- sanity/schemaTypes/blockContent.ts
+++ sanity/schemaTypes/blockContent.ts
@@ -1,4 +1,4 @@
-import { defineArrayMember, defineField, defineType } from 'sanity';
+import { defineArrayMember, defineType } from 'sanity';
 
 /**
  * This is the schema type for block content used in the post document type
