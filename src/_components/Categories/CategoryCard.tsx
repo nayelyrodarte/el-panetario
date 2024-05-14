29e301227031c8c4bd0dd795dc9d3f24a80727c9
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-05-14 15:56:53 -0400

    Add styling for category card

--- src/_components/Categories/CategoryCard.tsx
+++ src/_components/Categories/CategoryCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 interface CategoryCard {
   category: {
@@ -7,15 +8,36 @@ interface CategoryCard {
   };
 }
 
-const Card = styled.div``;
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.6rem;
+
+  padding: var(--spacing-md);
+  width: 10.5rem;
+
+  background: var(--secondary-color-soft);
+  color: var(--primary-color-contrast);
+  border-radius: var(--border-radius);
+`;
+
+const StyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+`;
+
 export default function CategoryCard({ category }: CategoryCard) {
   const { title, image } = category;
   return (
-    <div>
-      {image && (
-        <Image color="red" src={image} alt={title} width={60} height={60} />
-      )}
-      <h3>{title}</h3>
-    </div>
+    // TODO: Add routing when single recipe page is ready
+    <StyledLink href="/">
+      <Card>
+        {image && (
+          <Image color="red" src={image} alt={title} width={60} height={60} />
+        )}
+        <h3>{title}</h3>
+      </Card>
+    </StyledLink>
   );
 }
