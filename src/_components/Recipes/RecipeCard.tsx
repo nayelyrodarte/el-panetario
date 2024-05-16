e66e7c76308d2e5364fff0973a734538b226ac47
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-05-16 16:41:05 -0400

    Turn recipe card into link

--- src/_components/Recipes/RecipeCard.tsx
+++ src/_components/Recipes/RecipeCard.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const RecipeCardStyled = styled.article`
   display: flex;
   flex-direction: column;
   gap: var(--spacing-sm);
+
+  border-radius: var(--border-radius);
 
   width: 15rem;
   padding: 1rem;
@@ -18,8 +21,18 @@ const RecipeCardStyled = styled.article`
   p {
     font-weight: bold;
   }
+
+  &:hover,
+  &:focus-within {
+    background: var(--secondary-color-soft);
+    box-shadow: 0 0 0.25rem var(--secondary-color);
+  }
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`;
 //TODO; types
 export default function RecipeCard(props: any) {
   const { title, mainImage, imageUrl, slug, description } = props;
@@ -30,17 +43,20 @@ export default function RecipeCard(props: any) {
   }
 
   return (
-    <RecipeCardStyled>
-      {mainImage && (
-        <Image
-          width={220}
-          height={200}
-          src={imageUrl}
-          alt={mainImage?.asset?.alt || title}
-          objectFit="cover"
-        />
-      )}
-      <p>{title}</p>
-    </RecipeCardStyled>
+    //TODO: Update href when recipe page is ready
+    <StyledLink href="">
+      <RecipeCardStyled>
+        {mainImage && (
+          <Image
+            width={220}
+            height={200}
+            src={imageUrl}
+            alt={mainImage?.asset?.alt || title}
+            objectFit="cover"
+          />
+        )}
+        <p>{title}</p>
+      </RecipeCardStyled>
+    </StyledLink>
   );
 }
