476e1517d573cf87a20a8c039bffdd60defd99af
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-05-21 19:46:01 -0400

    Rearrange queries file

--- sanity/queries.ts
+++ sanity/queries.ts
@@ -1,6 +1,23 @@
 // Params should be passed via URL query params or by the client.fetch function,
 // for more info: https://www.sanity.io/docs/groq-parameters
 
+// LAYOUT
+
+export const getHomeData = `
+{'featured': *[_type == "category" && title == 'Featured']{
+ title,
+ 'recipes': *[_type == "recipe" && references(^._id)]{ title, mainImage, slug }[0..3]
+},
+'categories': *[_type == "category" && title != 'Featured']{
+ title,
+ _id,
+ 'image': image.asset->url,
+},
+}
+`;
+
+// RECIPES
+
 export const getAllRecipes = `
 *[_type == "recipe"]{
  title,
@@ -11,6 +28,20 @@ export const getAllRecipes = `
  slug
 }
 `;
+
+//TODO: Add related recipes (in same category)
+export const getRecipeBySlug = `
+*[_type == "recipe" && slug.current == $slug]{
+   title,
+   description,
+   mainImage,
+   'imageUrl': mainImage.asset->url,
+   ingredients,
+   method,
+ }[0]
+`;
+
+// CATEGORIES
 
 export const getAllCategories = `
 *[_type == "category"]{
@@ -23,29 +54,5 @@ export const getRecipeByCategoryId = `
 *[_type == "category" && $categoryId == _id]{
  title,
  'recipes': *[_type == "recipe" && references(^._id)]{ title, mainImage, slug }
-}
-`;
-
-//TODO: Add related recipes (in same category)
-export const getRecipeBySlug = `
-*[_type == "recipe" && slug.current == $slug]{
-   title,
-   description,
-   mainImage,
-   ingredients,
-   method,
- }[0]
-`;
-
-export const getHomeData = `
-{'featured': *[_type == "category" && title == 'Featured']{
- title,
- 'recipes': *[_type == "recipe" && references(^._id)]{ title, mainImage, slug }[0..3]
-},
-'categories': *[_type == "category" && title != 'Featured']{
- title,
- _id,
- 'image': image.asset->url,
-},
 }
 `;
