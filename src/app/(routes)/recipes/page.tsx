b5907b60e5bae313cd79912d25cc61236e346c9e
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-06-12 17:04:46 -0400

    Update types on recipes route

--- src/app/(routes)/recipes/page.tsx
+++ src/app/(routes)/recipes/page.tsx
@@ -2,19 +2,21 @@ import { Header } from '@/_components/Header';
 import { RecipeContainer } from '@/_components/Recipes/RecipeContainer';
 import { client } from '@/app/_api/client';
 
+import { Recipe } from '../../../../sanity.types';
 import {
   getAllRecipes,
   getRecipeByCategoryId,
 } from '../../../../sanity/queries';
 
-export default async function Recipes({
-  searchParams,
-}: {
-  searchParams: { category: string };
-}) {
-  //TODO: TYPES
-  let recipes: any = [];
-  let title = null;
+type Params = {
+  searchParams: {
+    category: string;
+  };
+};
+
+export default async function Recipes({ searchParams }: Params) {
+  let recipes: Recipe[] = [];
+  let title: string | null = null;
 
   if (searchParams?.category) {
     const data = await client.fetch(getRecipeByCategoryId, {
