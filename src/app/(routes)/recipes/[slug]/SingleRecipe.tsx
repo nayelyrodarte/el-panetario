e2379539d5a85e855c693ad623511da00df2962a
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-07-10 11:32:56 -0400

    Style single recipe header

--- src/app/(routes)/recipes/[slug]/SingleRecipe.tsx
+++ src/app/(routes)/recipes/[slug]/SingleRecipe.tsx
@@ -1,15 +1,61 @@
 'use client';
 
 import { PortableText } from '@portabletext/react';
+import Image from 'next/image';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 import { Header } from '@/_components/Header';
 import { components } from '@/utils/portableTextUtils';
 
 import { Recipe } from '../../../../../sanity.types';
 
+const StyledHeader = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: calc(var(--max-width) - 40rem);
+
+  gap: var(--spacing-sm);
+  padding: var(--spacing-md) 0;
+
+  img {
+    border-radius: var(--border-radius);
+    width: 100%;
+    height: 100%;
+  }
+
+  section {
+    position: relative;
+    display: grid;
+    grid-template-columns: 100%;
+    gap: var(--spacing-sm);
+    align-items: center;
+    justify-items: center;
+
+    padding: 0 var(--spacing-md);
+
+    box-sizing: border-box;
+
+    @media only screen and (min-width: 45em) {
+      grid-template-columns: repeat(2, 50%);
+      justify-items: start;
+      gap: var(--spacing-sm);
+    }
+  }
+
+  p,
+  h1 {
+    max-width: 70ch;
+  }
+
+  @media only screen and (min-width: 45em) {
+    height: 20rem;
+  }
+`;
+
 export function SingleRecipe(props: Recipe) {
-  const { title, description, ingredients = [], method } = props;
+  const { title, description, ingredients = [], method, imageUrl } = props;
   const [measurement, setMeasurement] = useState<'grams' | 'cups'>('grams');
   const [portion, setPortion] = useState(1);
 
@@ -21,8 +67,15 @@ export function SingleRecipe(props: Recipe) {
   return (
     <main>
       <Header>
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <StyledHeader>
+          <h2>{title}</h2>
+          <section>
+            {imageUrl && (
+              <Image src={imageUrl} alt={title} width={150} height={150} />
+            )}
+            <p>{description}</p>
+          </section>
+        </StyledHeader>
       </Header>
       <section>
         {ingredients &&
