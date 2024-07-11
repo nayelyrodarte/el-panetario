cc8b12e788ac6e7d7ca9ed9a379fe936aeac6159
Nayely Rodarte <nayelyrodarte@gmail.com>
2024-07-11 19:10:56 -0400

    Fix intro alignment

--- src/_components/Intro/Intro.tsx
+++ src/_components/Intro/Intro.tsx
@@ -7,8 +7,12 @@ import styled from 'styled-components';
 import ProfilePhoto from '../../_assets/img/NR2024-4_50.jpg';
 
 const Wrapper = styled.section`
+  padding: var(--spacing-md) var(--spacing-lg);
+`;
+
+const StyledIntro = styled.section`
+  margin: auto;
   max-width: var(--max-width);
-  margin: var(--spacing-lg) var(--spacing-md);
 
   display: flex;
   gap: var(--spacing-lg);
@@ -27,10 +31,10 @@ const StyledImage = styled(Image)`
   object-fit: cover;
   object-position: 0% 30%;
 
-  width: 90vw;
+  width: 85vw;
   height: 200px;
 
-  @media only screen and (min-width: 48em) {
+  @media only screen and (min-width: 64em) {
     object-position: 0% 20%;
     width: 350px;
     height: 350px;
@@ -49,20 +53,22 @@ const Text = styled.article`
 export function Intro() {
   return (
     <Wrapper>
-      <StyledImage src={ProfilePhoto} alt="Foto de perfil" />
-      <Text>
-        <Title>¡Hola! Soy Becky,</Title>
-        <p>
-          Aqui va la descripción acerca de ti. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo ipsum dolor
-          sit amet, consectetur adipiscing elit, sed do esmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-          nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          Puedes seguirme en Instagram, YouTube y Tiktok.
-        </p>
-      </Text>
+      <StyledIntro>
+        <StyledImage src={ProfilePhoto} alt="Foto de perfil" />
+        <Text>
+          <Title>¡Hola! Soy Becky,</Title>
+          <p>
+            Aqui va la descripción acerca de ti. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do esmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo Puedes seguirme en Instagram, YouTube y Tiktok.
+          </p>
+        </Text>
+      </StyledIntro>
     </Wrapper>
   );
 }
